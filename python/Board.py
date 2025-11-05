589c3d9427d6575720742b372deed136904437aa
skyshr <skyshr1995@gmail.com>
2025-11-05 21:07:04 +0900

    feat: 25/11/05 planning

--- python/Board.py
+++ python/Board.py
@@ -1,5 +1,24 @@
 from Player import Player
+from pieces import *
 import random
+
+# 0: White, 1: Black
+# 0: Rook, 1: Knight, 2: Bishop, 3: Queen, 4: King, 5: Bishop, 6: Knight, 7: Pawn
+
+# 중심 3을 기준으로 side만큼 보정
+piece_coords_x = {
+    'Rook': [0, 7],
+    'Knight': [1, 6],
+    'Bishop': [2, 5],
+    'Queen': [3],
+    'King': [4],
+    'Pawn': [0, 1, 2, 3, 4, 5, 6, 7],
+}
+
+# Board coordinates:
+# [x][y]
+# x => 0: A, 1: B, 2: C, 3: D, 4: E, 5: F, 6: G, 7: H
+# y => 0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8
 
 class Board:
     game_number = 0
@@ -8,7 +27,15 @@ class Board:
         self.game_number = Board.game_number
         self.playerA = playerA
         self.playerB = playerB
+        self.board = [[None for _ in range(8)] for _ in range(8)]
 
+        self.board[0][0] = Rook(0, 0, 0)
+        self.board[0][1] = Knight(0, 1, 0)
+        self.board[0][2] = Bishop(0, 2, 0)
+        self.board[0][3] = Queen(0, 3, 0)
+        self.board[0][4] = King(0, 4, 0)
+        self.board[0][5] = Bishop(0, 5, 0)
+        self.board[0][6] = Knight(0, 6, 0)
         side = random.randint(0, 1)
         self.playerA.register_side(side)
         self.playerB.register_side(side + 1)
