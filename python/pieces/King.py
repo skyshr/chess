e6e23ec256c8a1db74081817a5ca0cc900c41e2c
skyshr <skyshr1995@gmail.com>
2025-11-11 22:48:24 +0900

    feat: knight check dirs added

--- python/pieces/King.py
+++ python/pieces/King.py
@@ -1,6 +1,10 @@
 import sys
 import os
 
+from itertools import product
+
+import pieces
+
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from .Piece import Piece
 
@@ -33,6 +37,20 @@ class King(Piece):
                 if 0 <= nx < 8 and 0 <= ny < 8:
                     if opponent_attack_map[nx][ny] and (not board[nx][ny] or board[nx][ny].side != self.side):
                         cnt += 1
+
+        knight_dirs = [
+            (x, y) 
+            for a, b in [([-1, 1], [-2, 2]), ([-2, 2], [-1, 1])]
+            for x, y in product(a, b)
+        ]
+        for dx, dy in knight_dirs:
+            nx = self.x + dx
+            ny = self.y + dy
+            if 0 <= nx < 8 and 0 <= ny < 8:
+                piece = board[nx][ny]
+                if not piece or piece.side == self.side or not isinstance(piece, pieces.Knight):
+                    continue
+                cnt += 1
         return cnt
 # k = King(0, 0, 0)
 # k.possible_move()
